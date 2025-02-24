8f3721d2ff50f284b1ae40172bc44ec710d592bb
fatemeh soufian <fatemehsoufian2@gmail.com>
2025-02-25 01:05:22 +0200

    add visualization result file

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,11 @@
 import os
-import pandas as pd
 from add_attributes import add_timestamp_and_id_to_csv
 from extract import extract 
-from remove import remove_subfolders
+from remove import remove_subfolders, delete_ibi_and_tags_files
 from integrating import integrate_data
+from resampling import resampling
+from comparison_visualization import compare_original_resampled
+import pandas as pd
 
 base_path = "dmp-dataset\\Stress_dataset"
 nurses_id = ["5C","6B","6D","7A","7E","8B","15","83","94","BG","CE","DF","E4","EG","F5"]
@@ -21,7 +23,21 @@ def merge_all():
                         csv_file = file.path
                         add_timestamp_and_id_to_csv(csv_file,nurse)
 
-extract(".")
-merge_all()
-remove_subfolders()
+# extract(".")
+# merge_all()
+# remove_subfolders()
+# delete_ibi_and_tags_files("dmp-dataset\Stress_dataset")
+# resampling()
 integrate_data()
+
+# original = pd.read_csv("D:\dmp-dataset\Stress_dataset\ACC_1.csv")
+# resample = pd.read_csv("D:/dmp-dataset/Resampled/resampled_ACC_1.csv")
+# compare_original_resampled(original,resample,"ACC")
+
+# original = pd.read_csv("D:\dmp-dataset\Stress_dataset\BVP_3705.csv")
+# resample = pd.read_csv("D:/dmp-dataset/Resampled/resampled_BVP_3705.csv")
+# compare_original_resampled(original,resample,"BVP")
+
+# original = pd.read_csv("D:\dmp-dataset\Stress_dataset\HR_3770.csv")
+# resample = pd.read_csv("D:/dmp-dataset/Resampled/resampled_HR_3770.csv")
+# compare_original_resampled(original,resample,"HR")
